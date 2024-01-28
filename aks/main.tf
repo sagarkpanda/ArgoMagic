f26f910b30d33a8c19a1b4196265dfc3b4a2ada8
Sagar <42873729+sagarkrp@users.noreply.github.com>
2024-01-28 17:38:35 +0530

    Create main.tf

---
+++ aks/main.tf
@@ -0,0 +1,49 @@
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "k8s_rg" {
+  name     = "k8s_rg"
+  location = "Central India"
+}
+
+resource "azurerm_kubernetes_cluster" "aks_cluster" {
+  name                = "argo_cluster"
+  location            = azurerm_resource_group.k8s_rg.location
+  resource_group_name = azurerm_resource_group.k8s_rg.name
+  dns_prefix          = "argocluster"
+
+  default_node_pool {
+    name       = "default"
+    node_count = 1
+    vm_size    = "Standard_DS2_v3"
+  }
+
+  identity {
+    type = "SystemAssigned"
+  }
+
+  network_profile {
+    network_plugin = "azure"
+    network_policy = "calico"
+  }
+
+  tags = {
+    environment = "cprime"
+    managed_by  = "Terraform"
+  }
+}
+
+resource "helm_release" "argo_cd" {
+  name       = "argocd"
+  repository = "https://argoproj.github.io/argo-helm"
+  chart      = "argo-cd"
+  namespace  = "argocd"
+
+  set {
+    name  = "server.service.type"
+    value = "LoadBalancer"
+  }
+
+  depends_on = [azurerm_kubernetes_cluster.aks_cluster]
+}
